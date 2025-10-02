79edc9ed26d3c04bb2ed288237cc99d58918b67d
Mads Studsgaard Rasmussen <madsrasmussendev@gmail.com>
2025-10-02 22:48:27 +0200

    update(nvim): update nvim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,7 +1,21 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=4")
-vim.cmd("set softtabstop=4")
-vim.cmd("set shiftwidth=4")
-vim.cmd("set number")
+-- Setup globals
 vim.g.mapleader = " "
-vim.cmd.colorscheme("habamax")
+
+-- Setup options
+vim.opt.number = true
+
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+
+-- Setup keymaps
+vim.keymap.set('n', '<C-h>', '<C-w>h', { desc = 'Move to left pane' })
+vim.keymap.set('n', '<C-l>', '<C-w>l', { desc = 'Move to right pane' })
+vim.keymap.set('n', '<C-j>', '<C-w>j', { desc = 'Move to pane below' })
+vim.keymap.set('n', '<C-k>', '<C-w>k', { desc = 'Move to pane above' })
+
+vim.keymap.set('n', '<leader>e', vim.cmd.Ex, { desc = 'Open explorer' })
+
+-- Setup colorscheme
+vim.cmd.colorscheme('habamax')
