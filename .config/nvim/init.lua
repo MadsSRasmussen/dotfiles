6e1f7f381968490568b002f99af2bd2b6bafef2c
Mads Studsgaard Rasmussen <madsrasmussendev@gmail.com>
2025-10-04 22:11:47 +0200

    feat(nvim): add native lsp support

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -9,13 +9,19 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 
+vim.opt.winborder = 'single'
+
 -- Setup keymaps
 vim.keymap.set('n', '<C-h>', '<C-w>h', { desc = 'Move to left pane' })
 vim.keymap.set('n', '<C-l>', '<C-w>l', { desc = 'Move to right pane' })
 vim.keymap.set('n', '<C-j>', '<C-w>j', { desc = 'Move to pane below' })
 vim.keymap.set('n', '<C-k>', '<C-w>k', { desc = 'Move to pane above' })
 
-vim.keymap.set('n', '<leader>e', vim.cmd.Ex, { desc = 'Open explorer' })
+vim.keymap.set('n', '<leader>e', ':Explore<CR>', { desc = 'Open explorer' })
+vim.keymap.set('n', '<leader>so', ':source $MYVIMRC<CR>', { desc = 'Source vimrc' })
 
 -- Setup colorscheme
-vim.cmd.colorscheme('habamax')
+vim.cmd.colorscheme('default')
+
+-- Setup Lsp
+vim.lsp.enable('lua_ls')
